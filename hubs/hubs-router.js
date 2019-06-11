5eb5d8e603ae0dd897f42d2fd44aee4d7c7ff289
Elvis Ibarra <ielvisd@gmail.com>
2019-06-10 20:03:25 -0700

    guided solution

--- hubs/hubs-router.js
+++ hubs/hubs-router.js
@@ -5,117 +5,171 @@ const Messages = require('../messages/messages-model.js');
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+	console.log('Hubs Router!');
+	next();
+});
+
 // this only runs if the url has /api/hubs in it
 router.get('/', async (req, res) => {
-  try {
-    const hubs = await Hubs.find(req.query);
-    res.status(200).json(hubs);
-  } catch (error) {
-    // log error to server
-    console.log(error);
-    res.status(500).json({
-      message: 'Error retrieving the hubs',
-    });
-  }
+	try {
+		const hubs = await Hubs.find(req.query);
+		res.status(200).json(hubs);
+	} catch (error) {
+		// log error to server
+		console.log(error);
+		res.status(500).json({
+			message: 'Error retrieving the hubs'
+		});
+	}
 });
 
 // /api/hubs/:id
 
-router.get('/:id', async (req, res) => {
-  try {
-    const hub = await Hubs.findById(req.params.id);
+router.get('/:id', validateId, async (req, res) => {
+	// try {
+	// 	const hub = await Hubs.findById(req.params.id);
 
-    if (hub) {
-      res.status(200).json(hub);
-    } else {
-      res.status(404).json({ message: 'Hub not found' });
-    }
-  } catch (error) {
-    // log error to server
-    console.log(error);
-    res.status(500).json({
-      message: 'Error retrieving the hub',
-    });
-  }
+	// 	if (hub) {
+	// 		res.status(200).json(hub);
+	// 	} else {
+	// 		res.status(404).json({ message: 'Hub not found' });
+	// 	}
+	// } catch (error) {
+	// 	// log error to server
+	// 	console.log(error);
+	// 	res.status(500).json({
+	// 		message: 'Error retrieving the hub'
+	// 	});
+	// }
+
+	res.status(200).json(req.hub);
 });
 
-router.post('/', async (req, res) => {
-  try {
-    const hub = await Hubs.add(req.body);
-    res.status(201).json(hub);
-  } catch (error) {
-    // log error to server
-    console.log(error);
-    res.status(500).json({
-      message: 'Error adding the hub',
-    });
-  }
+router.post('/', requiredBody, async (req, res) => {
+	try {
+		const hub = await Hubs.add(req.body);
+		res.status(201).json(hub);
+	} catch (error) {
+		// log error to server
+		console.log(error);
+		res.status(500).json({
+			message: 'Error adding the hub'
+		});
+	}
 });
 
-router.delete('/:id', async (req, res) => {
-  try {
-    const count = await Hubs.remove(req.params.id);
-    if (count > 0) {
-      res.status(200).json({ message: 'The hub has been nuked' });
-    } else {
-      res.status(404).json({ message: 'The hub could not be found' });
-    }
-  } catch (error) {
-    // log error to server
-    console.log(error);
-    res.status(500).json({
-      message: 'Error removing the hub',
-    });
-  }
+router.delete('/:id', validateId, async (req, res) => {
+	try {
+		const count = await Hubs.remove(req.params.id);
+		if (count > 0) {
+			res.status(200).json({ message: 'The hub has been nuked' });
+		} else {
+			res.status(404).json({ message: 'The hub could not be found' });
+		}
+	} catch (error) {
+		// log error to server
+		console.log(error);
+		res.status(500).json({
+			message: 'Error removing the hub'
+		});
+	}
 });
 
-router.put('/:id', async (req, res) => {
-  try {
-    const hub = await Hubs.update(req.params.id, req.body);
-    if (hub) {
-      res.status(200).json(hub);
-    } else {
-      res.status(404).json({ message: 'The hub could not be found' });
-    }
-  } catch (error) {
-    // log error to server
-    console.log(error);
-    res.status(500).json({
-      message: 'Error updating the hub',
-    });
-  }
+router.put('/:id', validateId, requiredBody, async (req, res) => {
+	try {
+		const hub = await Hubs.update(req.params.id, req.body);
+		if (hub) {
+			res.status(200).json(hub);
+		} else {
+			res.status(404).json({ message: 'The hub could not be found' });
+		}
+	} catch (error) {
+		// log error to server
+		console.log(error);
+		res.status(500).json({
+			message: 'Error updating the hub'
+		});
+	}
 });
 
 // add an endpoint that returns all the messages for a hub
 // this is a sub-route or sub-resource
-router.get('/:id/messages', async (req, res) => {
-  try {
-    const messages = await Hubs.findHubMessages(req.params.id);
+router.get('/:id/messages', validateId, async (req, res) => {
+	try {
+		const messages = await Hubs.findHubMessages(req.params.id);
 
-    res.status(200).json(messages);
-  } catch (error) {
-    // log error to server
-    console.log(error);
-    res.status(500).json({
-      message: 'Error getting the messages for the hub',
-    });
-  }
+		res.status(200).json(messages);
+	} catch (error) {
+		// log error to server
+		console.log(error);
+		res.status(500).json({
+			message: 'Error getting the messages for the hub'
+		});
+	}
 });
 
 // add an endpoint for adding new message to a hub
-router.post('/:id/messages', async (req, res) => {
-  const messageInfo = { ...req.body, hub_id: req.params.id };
+router.post('/:id/messages', validateId, requiredBody, async (req, res) => {
+	const messageInfo = { ...req.body, hub_id: req.params.id };
 
-  try {
-    const message = await Messages.add(messageInfo);
-    res.status(210).json(message);
-  } catch (error) {
-    // log error to server
-    console.log(error);
-    res.status(500).json({
-      message: 'Error getting the messages for the hub',
-    });
-  }
+	try {
+		const message = await Messages.add(messageInfo);
+		res.status(210).json(message);
+	} catch (error) {
+		// log error to server
+		console.log(error);
+		res.status(500).json({
+			message: 'Error getting the messages for the hub'
+		});
+	}
 });
+
+// function validateId(req, res, next) {
+// 	const { id } = req.params;
+// 	Hubs.findById(id)
+// 		.then(hub => {
+// 			if (hub) {
+// 				req.hub = hub;
+// 				next();
+// 			} else {
+// 				// We like very specific error messages
+// 				res.status(404).json({ message: 'Invalid id; hub not found' });
+// 			}
+// 		})
+// 		.catch(error => {
+// 			// log error to server
+// 			console.log(error);
+// 			res.status(500).json({ message: 'Failed to process request' });
+// 		});
+// }
+
+// or with async/await
+async function validateId(req, res, next) {
+	try {
+		const { id } = req.params;
+		const hub = await Hubs.findById(id);
+		if (hub) {
+			req.hub = hub;
+			next();
+		} else {
+			// We like very specific error messages
+			// res.status(404).json({ message: 'Invalid id; hub not found' })
+			next({ message: 'Invalid id; hub not found' });
+		}
+	} catch (error) {
+		// log error to server
+		console.log(error);
+		res.status(500).json({ message: 'Failed to process request' });
+	}
+}
+
+function requiredBody(req, res, next) {
+	if (req.body && Object.keys(req.body).length > 0) {
+		next();
+	} else {
+		next({ message: 'Please include request body' });
+	}
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,63 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev')); // one of the options for the display format is 'dev'
+server.use(methodLogger);
+server.use(addName);
+// server.use(moodyGateKeeper);
 
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+	const nameInsert = req.name ? ` ${req.name}` : '';
 
-  res.send(`
+	res.send(`
     <h2>Lambda Hubs API</h2>
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
+});
+
+// just like that, the homies have become the three amigos (Joshua Keslar Web17)
+function methodLogger(req, res, next) {
+	console.log(`${req.method} Request`);
+	next();
+}
+
+// Add a piece of middleware that add info to the request object
+function addName(req, res, next) {
+	req.name = req.name || 'Nathan'; // checks if req.name is on the object, if not hardcoded to 'Nathan'
+	next();
+}
+
+function lockout(req, res, next) {
+	res.status(403).json({ message: 'API lockout!' });
+}
+
+// You Do (estimated 5 min to complete)
+// Ask students to write and use custom middleware called `moodyGateKeeper` that
+// returns status code 403 and the message 'you shall not pass!'
+// when the seconds on the clock are multiples of 3 and call next()
+// for all other times.
+
+function moodyGateKeeper(req, res, next) {
+	const seconds = new Date().getSeconds();
+	if (seconds % 3 == 0) {
+		res.status(403).json({ message: 'API lockout' });
+	}
+	next();
+}
+
+// error handling middleware
+server.use((error, req, res, next) => {
+	// here you could inspect the error and decide how to respond
+	res.status(400).json({ message: 'Bad Panda!', error });
 });
 
 module.exports = server;
